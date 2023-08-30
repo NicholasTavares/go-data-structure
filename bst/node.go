1255124ff8ba02b104fe5984b82cdfcdddf90461
NicholasTavares <nicholasbalby@hotmail.com>
2023-08-29 22:36:44 -0300

    feat: node bst

---
+++ bst/node.go
@@ -0,0 +1,15 @@
+package bst
+
+type Node struct {
+    Value int
+    Left  *Node
+	Right  *Node
+}
+
+func NewNode(value int) *Node {
+    return &Node{
+        Value: value,
+        Left:  nil,
+		Right:  nil,
+    }
+}
